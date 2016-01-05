33c524d241a64592fe7f0eadee477bf73d38790c
DEVTYPE <Informatik.on.GitHub@gmail.com>
2016-01-05 22:20:40 +0600

    Rectangular callout

--- DrawingOnView/DrawingOnView/CustomView.m
+++ DrawingOnView/DrawingOnView/CustomView.m
@@ -10,6 +10,9 @@
 
 @implementation CustomView
 
+/**
+ @see http://devtype.blogspot.com/2016/01/Kak-rabotaet-funktsiya-NSInsetRect.html
+ */
 - (void)testInsetRectWithRect:(NSRect)aDirtyRect
 {
     [[NSColor redColor] setFill];
@@ -21,11 +24,36 @@
     NSRectFill(theContentRect);
 }
 
+/**
+ @see http://devtype.blogspot.com/2016/01/Krivye-Beze-v-Cocoa.html
+ */
+- (void)testBezierWithRect:(NSRect)aDirtyRect
+{
+    NSBezierPath *thePath = [NSBezierPath bezierPath];
+    [thePath moveToPoint:NSMakePoint(NSMidX(aDirtyRect), NSMaxY(aDirtyRect))];
+    
+    NSUInteger theArrWidth = 32, theArrHeight = 24;
+    [thePath lineToPoint:NSMakePoint(NSMidX(aDirtyRect) + theArrWidth / 2, NSMaxY(aDirtyRect) - theArrHeight)];
+    [thePath lineToPoint:NSMakePoint(NSMaxX(aDirtyRect), NSMaxY(aDirtyRect) - theArrHeight)];
+    [thePath lineToPoint:NSMakePoint(NSMaxX(aDirtyRect), NSMinY(aDirtyRect))];
+    [thePath lineToPoint:NSMakePoint(NSMinX(aDirtyRect), NSMinY(aDirtyRect))];
+    [thePath lineToPoint:NSMakePoint(NSMinX(aDirtyRect), NSMaxY(aDirtyRect) - theArrHeight)];
+    [thePath lineToPoint:NSMakePoint(NSMidX(aDirtyRect) - theArrWidth / 2, NSMaxY(aDirtyRect) - theArrHeight)];
+    
+    [thePath closePath];
+    
+    [[NSColor colorWithDeviceWhite:1.0 alpha:1.0] setFill];
+    [thePath fill];
+    
+    [[NSColor blueColor] setStroke];
+    [thePath stroke];
+}
+
 - (void)drawRect:(NSRect)aDirtyRect
 {
     [super drawRect:aDirtyRect];
     
-    [self testInsetRectWithRect:aDirtyRect];
+    [self testBezierWithRect:aDirtyRect];
 }
 
 @end
